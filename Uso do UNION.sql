becec9780d7671b39cb2aaab9c69fc97e9ef2dd9
Fred Oliveira <fredleonardo0@gmail.com>
2021-07-30 16:32:18 -0300

    Organização do código

--- Uso do UNION.sql
+++ Uso do UNION.sql
@@ -1,52 +1,52 @@
 --EXEMPLO 1 USANDO UNION
-    SELECT city
-    FROM customers
+SELECT city
+FROM customers
 
 UNION
 
-    SELECT city
-    FROM suppliers
+SELECT city
+FROM suppliers
 ORDER BY city
 
 --************************--
 --EXEMPLO 1 USANDO UNION ALL
-    SELECT 'CLI',
+SELECT 'CLI',
         city
-    FROM customers
+FROM customers
 
 UNION ALL
 
-    SELECT 'FORNEC',
+SELECT 'FORNEC',
         city
-    FROM suppliers
+FROM suppliers
 ORDER BY city
 
 --********************--
 --EXEMPLO 2 USANDO UNION
-    SELECT city,
-        country
-    FROM customers
-    WHERE country = 'Germany'
+SELECT city,
+       country
+FROM customers
+WHERE country = 'Germany'
 
 UNION
 
-    SELECT city,
-        country
-    FROM suppliers
-    WHERE country = 'Germany'
+SELECT city,
+       country
+FROM suppliers
+WHERE country = 'Germany'
 ORDER BY city
 
 --************************--
 --EXEMPLO 2 USANDO UNION ALL
-    SELECT city,
-        country
-    FROM customers
-    WHERE country = 'Germany'
+SELECT city,
+       country
+FROM customers
+WHERE country = 'Germany'
 
 UNION ALL
 
-    SELECT city,
-        country
-    FROM suppliers
-    WHERE country = 'Germany'
+SELECT city,
+       country
+FROM suppliers
+WHERE country = 'Germany'
 ORDER BY city
