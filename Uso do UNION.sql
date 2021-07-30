75a6afb15f7166e25535bd4ae3be25de3701cd2a
Fred Oliveira <fredleonardo0@gmail.com>
2021-07-30 16:08:47 -0300

    Adicionando o arquivo UNION.sql

---
+++ Uso do UNION.sql
@@ -0,0 +1,53 @@
+
+--EXEMPLO 1 USANDO UNION
+    SELECT city
+    FROM customers
+
+UNION
+
+    SELECT city
+    FROM suppliers
+ORDER BY city
+
+--************************--
+--EXEMPLO 1 USANDO UNION ALL
+    SELECT 'CLI',
+        city
+    FROM customers
+
+UNION ALL
+
+    SELECT 'FORNEC',
+        city
+    FROM suppliers
+ORDER BY city
+
+--********************--
+--EXEMPLO 2 USANDO UNION
+    SELECT city,
+        country
+    FROM customers
+    WHERE country = 'Germany'
+
+UNION
+
+    SELECT city,
+        country
+    FROM suppliers
+    WHERE country = 'Germany'
+ORDER BY city
+
+--************************--
+--EXEMPLO 2 USANDO UNION ALL
+    SELECT city,
+        country
+    FROM customers
+    WHERE country = 'Germany'
+
+UNION ALL
+
+    SELECT city,
+        country
+    FROM suppliers
+    WHERE country = 'Germany'
+ORDER BY city
