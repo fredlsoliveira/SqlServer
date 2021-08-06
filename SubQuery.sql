334aa68a52ba29afbd3719a22865dbcd4d449249
Fred Oliveira <fredleonardo0@gmail.com>
2021-08-06 12:03:20 -0300

    Adicionado o arquivo 'Referências (links).txt' e inserias linhas no arquivo 'SubQuery.sql'

--- SubQuery.sql
+++ SubQuery.sql
@@ -29,3 +29,53 @@ UPDATE pedido SET total =
 	SELECT SUM(d.UnitPrice*d.Quantity) FROM NORTHWND.dbo.[Order Details] d
 	WHERE num_ped = d.OrderID
 )
+
+--APRESENTANDO OS CLIENTES QUE FIZERAM COMPRAS ANTES DA DATA ATUAL (GETDATE())
+SELECT	c.cod_cli,
+		c.cli_nome
+FROM cliente c
+WHERE c.cod_cli IN
+(
+	SELECT p.cod_cliente
+	FROM pedido p
+	WHERE p.data < GETDATE()
+)
+
+--CONSULTA TRZER O NOME CLIENTE NA TABELA DE PEDIDO
+SELECT	p.num_ped,
+		p.data,
+		p.cod_cliente,
+		(	
+		SELECT c.cli_nome 
+		FROM cliente c
+		WHERE p.cod_cliente = c.cod_cli
+		) 
+		AS NomeCliente
+FROM pedido p
+
+--CONSULTA SOMAR TODO VALOR COMPRADO PELO CLIENTE
+SELECT	p.cod_cliente,
+		(
+		SELECT c.cli_nome
+		FROM cliente c
+		WHERE p.cod_cliente = c.cod_cli
+		)
+		AS NomeCliente,
+		SUM(p.total) AS ValorTotal		 
+FROM pedido p
+GROUP BY p.cod_cliente 
+
+--CONSULTA MOSTRA TODOS OS CLIENTES E VALOR DE COMPRAS
+SELECT	c.cod_cli,
+		(
+		SELECT	ISNULL(SUM(p.total),0)				 
+		FROM pedido p
+		WHERE c.cod_cli = p.cod_cliente
+		)
+		AS ValorTotal
+FROM cliente c
+GROUP BY c.cod_cli
+
+--EXCLUINDO CLIENTE SEM PEDIDO
+DELETE FROM cliente
+WHERE cod_cli NOT IN (SELECT p.cod_cliente FROM pedido p)
