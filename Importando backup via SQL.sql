1d887362804125cebcc1594816ddcb6997925344
Fred Oliveira <fredleonardo0@gmail.com>
2021-09-08 15:30:01 -0300

    Adição dos comentários

--- Importando backup via SQL.sql
+++ Importando backup via SQL.sql
@@ -1,5 +1,7 @@
+/* MOSTRA DETALHES DO ARQUIVO A SER RESTAURADO */
 RESTORE FILELISTONLY FROM DISK = N'/home/fredoliveira/Documentos/curso_bk1.bak'
 
+/* RESTAURANDO O BACKUP */
 RESTORE DATABASE curso
 FROM DISK = N'/home/fredoliveira/Documentos/curso_bk1.bak'
 WITH MOVE 'curso' TO '/var/opt/mssql/data/curso.mdf',
